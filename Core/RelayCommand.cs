4c20c6d2c72e7e95f11e449ea0307f1c2bb79463
micha <michael.lurtz@outlook.com>
2022-12-10 22:33:46 +0100

    RelayCommand

--- Core/RelayCommand.cs
+++ Core/RelayCommand.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Interface.Core
 {
-    internal class RelayCommand
+    internal class RelayCommand : ICommand
     {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute,Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) 
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter) 
+        {
+            _execute(parameter);
+        }
+
     }
 }
