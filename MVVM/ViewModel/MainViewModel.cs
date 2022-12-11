7bce25bf7290d2655663a956998fc477da4c6992
micha <michael.lurtz@outlook.com>
2022-12-11 11:14:07 +0100

    complete - ModernUI

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Interface.Core;
+using Interface.MVVM.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,38 @@ namespace Interface.MVVM.ViewModel
 {
     internal class MainViewModel : ObservalbleOjects
     {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM{ get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainViewModel()
         {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+            CurrentView = HomeVM;
 
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+                        {
+                            CurrentView = DiscoveryVM;
+                        });
         }
     }
 }
