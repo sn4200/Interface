58953bf1d6173deb72d70704a11c4995f3460308
micha <michael.lurtz@outlook.com>
2022-12-11 10:13:31 +0100

    Config NavBarTop

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Interface.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservalbleOjects
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
